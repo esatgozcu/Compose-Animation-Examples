9a59f2d99ce7b9fc47d8d56ae7f24813155d209c
Esat GÖZCÜ <esatgozcu@gmail.com>
2023-03-14 10:45:09 +1000

    0.0.2
    Home Page has been created

--- app/src/main/java/com/esatgozcu/animationexamples/ui/view/HomePage.kt
+++ app/src/main/java/com/esatgozcu/animationexamples/ui/view/HomePage.kt
@@ -1,24 +1,51 @@
 package com.esatgozcu.animationexamples.ui.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.esatgozcu.animationexamples.R
 import com.esatgozcu.animationexamples.ui.theme.AnimationExamplesTheme
 
 @Composable
 fun HomePage(navController: NavController) {
     Column(modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
     ) {
-        Text(text = "SnowFlake")
+        HomePageItem(title = "Snow Flake", image = R.drawable.ic_snow_flake){
+
+        }
+        HomePageItem(title = "Paper Plane", image = R.drawable.ic_paper_plane){
+
+        }
+    }
+}
+
+@Composable
+fun HomePageItem(title:String,
+                 image:Int,
+                 click: (()->Unit)){
+    Row(horizontalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = Modifier.clickable {
+            click()
+        }.padding(10.dp),
+    ){
+        Text(text = title)
+        Image(
+            modifier = Modifier.size(30.dp),
+            painter = painterResource(id = image),
+            contentDescription = ""
+        )
     }
 }
 
